38e1ab6d344f8bbcd0b5d8e08643029283dd9944
Jitugopale <jiteshgopale26@gmail.com>
2024-11-30 16:03:33 +0530

    first commit

---
+++ Backend/db.js
@@ -0,0 +1,11 @@
+
+const mongoose = require("mongoose");
+
+const connectToMongo = async () => {
+  await mongoose
+    .connect("mongodb+srv://mongodb:mongodb@mycluster.cq58k.mongodb.net/?retryWrites=true&w=majority&appName=MyCluster")
+    .then(() => console.log("DB Connected"))
+    .catch((err) => console.error("DB Connection Error: ", err));
+};
+
+module.exports = connectToMongo;
